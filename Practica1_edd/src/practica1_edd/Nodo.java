b8b33e40d7aefc32cbdb3665c7fe7ad48f3cbd32
BryanAlexander <BryanAlexander@192.168.1.13>
2015-07-31 12:20:52 -0600

    hola

---
+++ Practica1_edd/src/practica1_edd/Nodo.java
@@ -0,0 +1,19 @@
+
+
+package practica1_edd;
+
+
+public class Nodo {
+    
+    Nodo next;
+    Nodo back;
+    Objeto o;
+    
+    public Nodo()
+    {
+        next=null;
+        back=null;
+        o=new Objeto();
+    }
+    
+}

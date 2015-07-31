c069a9d3c3e9f4c1a2b72a15f4f4814ca78323b9
BryanAlexander <BryanAlexander@192.168.1.13>
2015-07-31 12:20:45 -0600

    hola

---
+++ Practica1_edd/src/practica1_edd/Objeto.java
@@ -0,0 +1,17 @@
+
+package practica1_edd;
+
+public class Objeto {
+    
+    public String nombre;
+    public int tipo;
+    public String img;
+    
+    public Objeto()
+    {
+        nombre="";
+        tipo=0;
+        img="";
+    }
+    
+}

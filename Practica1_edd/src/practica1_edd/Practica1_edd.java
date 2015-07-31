4009a159e253422356bfcd5d4ad2acc9c3f7578d
BryanAlexander <BryanAlexander@192.168.1.13>
2015-07-31 12:38:07 -0600

    hola

--- Practica1_edd/src/practica1_edd/Practica1_edd.java
+++ Practica1_edd/src/practica1_edd/Practica1_edd.java
@@ -4,6 +4,20 @@ public class Practica1_edd {
     
     public static void main(String[] args) {
         
+        Lista L=new Lista();
+        Objeto a=new Objeto();
+        a.nombre="a";
+        Objeto b=new Objeto();
+        b.nombre="b";
+        Objeto c=new Objeto();
+        c.nombre="c";
+        L.Insertar(a);
+        L.Insertar(b);
+        L.Insertar(c);
+        L.mostrar();
+       // L.Eliminar(a);
+        //L.mostrar();
+        
     }
     
 }

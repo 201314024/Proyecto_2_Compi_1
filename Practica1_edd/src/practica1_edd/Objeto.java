646030a438af4c9ba7bb5cba672f3beabf7bb71a
BryanAlexander <BryanAlexander@Bryan-PC>
2015-08-09 05:23:17 -0600

    hola

--- Practica1_edd/src/practica1_edd/Objeto.java
+++ Practica1_edd/src/practica1_edd/Objeto.java
@@ -6,12 +6,20 @@ public class Objeto {
     public String nombre;
     public int tipo;
     public String img;
+    public javax.swing.JLabel imagen=new javax.swing.JLabel();
     
     public Objeto()
     {
         nombre="";
         tipo=0;
         img="";
+        imagen.setText("");
+    }
+    public void imagen(String pic)
+    {
+        img=pic;
+        imagen.setIcon(new javax.swing.ImageIcon(getClass().getResource("/"+pic)));
+        imagen.setSize(10, 10);
     }
     
 }

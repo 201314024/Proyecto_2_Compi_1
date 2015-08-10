5a1b1cc8629a2c037c5fafd7488eb3c6b8e122eb
BryanAlexander <BryanAlexander@Bryan-PC>
2015-08-10 16:54:57 -0600

    hola

--- Practica1_edd/src/practica1_edd/Objeto.java
+++ Practica1_edd/src/practica1_edd/Objeto.java
@@ -7,13 +7,22 @@ public class Objeto {
     public int tipo;
     public String img;
     public javax.swing.JLabel imagen=new javax.swing.JLabel();
+    public int x;
+    public int y;
     
     public Objeto()
     {
+        x=0;
+        y=0;
         nombre="";
         tipo=0;
         img="";
         imagen.setText("");
+        imagen.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                imagenMouseClicked(evt);
+            }
+        });
     }
     public void imagen(String pic)
     {
@@ -21,5 +30,8 @@ public class Objeto {
         imagen.setIcon(new javax.swing.ImageIcon(getClass().getResource("/"+pic)));
         imagen.setSize(10, 10);
     }
+    public void imagenMouseClicked(java.awt.event.MouseEvent evt) {                                     
+       System.out.println("("+x+","+y+")");
+    }  
     
 }

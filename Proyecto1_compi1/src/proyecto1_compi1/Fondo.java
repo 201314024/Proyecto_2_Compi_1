e986cff9da63a5cc862b16925e1f393999c5f9e8
BryanAlexander <BryanAlexander@Bryan-PC>
2015-08-27 20:40:27 -0600

    hola

---
+++ Proyecto1_compi1/src/proyecto1_compi1/Fondo.java
@@ -0,0 +1,14 @@
+package proyecto1_compi1;
+
+public class Fondo {
+    
+    public String nombre;
+    public String path;
+    public String tipo;
+    public Fondo()
+    {
+        nombre="";
+        path="";
+        tipo="";
+    }
+}

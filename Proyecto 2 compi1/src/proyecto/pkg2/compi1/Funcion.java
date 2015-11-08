cbd9e0056da6f96a9eb8814b90ed447a6573f028
BryanAlexander <BryanAlexander@Bryan-PC>
2015-11-08 17:55:01 -0600

    hola

---
+++ Proyecto 2 compi1/src/proyecto/pkg2/compi1/Funcion.java
@@ -0,0 +1,16 @@
+
+package proyecto.pkg2.compi1;
+
+public class Funcion {
+    
+    
+    public String nombre="";
+    public int param=0;
+    
+    public Funcion()
+    {
+        nombre="";
+        param=0;
+    }
+    
+}
---
+++ Proyecto 2 compi1/src/proyecto/pkg2/compi1/variable.java
@@ -0,0 +1,15 @@
+
+package proyecto.pkg2.compi1;
+
+
+public class variable {
+    
+            public String nombre="";
+            public int valor=0;
+            public String funcion="";
+            public variable()
+            {
+                nombre="";
+                valor=0;
+            }
+}
